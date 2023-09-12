03472d513f03bddb4a0577138cf4ecf1df3f513b
James <89285665+JimboDaDuck@users.noreply.github.com>
2023-09-12 23:50:07 +1000

    Update JSimple.cpp
    Fixed an issue with having 2 CreateInput()'s right after another
    Simplified the source more

--- JSimple.cpp
+++ JSimple.cpp
@@ -1,42 +1,36 @@
-#include <iostream>
 #include "JSimple.hpp"
-#include <fstream>
-#include <vector>
-#include <string>
-#include <random>
-using string = JSimple::string;
-template <typename T>
-using vector = JSimple::vector<T>;
 
 void Print(string String)
 {
-    std::cout << String;
+    cout << String;
 }
 
 string GetInput()
 {
     string userInput;
-    std::cin >> userInput;
+    cin >> userInput;
     return userInput;
 }
 
 string CreateInput(const string& prompt) {
     string input;
-    Print(prompt);
-    std::cin >> input;
+    cout << prompt;
+    cin >> input;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     return input;
 }
+
 
 vector<string> ReadFileToList(const string& filename) {
     vector<string> lines;
 
-    std::ifstream file(filename);
+    ifstream file(filename);
     if (!file.is_open()) {
         return lines;
     }
 
     string line;
-    while (std::getline(file, line)) {
+    while (getline(file, line)) {
         lines.push_back(line);
     }
 
@@ -48,13 +42,13 @@ vector<string> ReadFileToList(const string& filename) {
 string ReadFileToString(const string& filename) {
     string lines;
 
-    std::ifstream file(filename);
+    ifstream file(filename);
     if (!file.is_open()) {
         return lines;
     }
 
     string line;
-    while (std::getline(file, line)) {
+    while (getline(file, line)) {
         lines = lines + "\n" + line;
     }
 
@@ -65,31 +59,43 @@ string ReadFileToString(const string& filename) {
 
 template <typename T>
 void WriteToFile(const string& filename, const T& data) {
-    std::ofstream outFile(filename, std::ios::app);
+    ofstream outFile(filename, ios::app);
     if (!outFile.is_open()) {
         return;
     }
 
-    outFile << data << std::endl;
+    outFile << data << endl;
     outFile.close();
 }
 
-string StringListToString(vector<string> stringList) {
-    string string;
+string ListToString(vector<string> stringList) {
+    string result;
 
-    for (const std::string& str : stringList) {
-        string += str;
+    for (const string& str : stringList) {
+        result += str;
     }
 
-    std::cout << string << std::endl;
-    return string;
+    return result;
 }
 
-float RandomFloat(float min, float max) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
+vector<string> StringToList(string input) {
+    vector<string> words;
+    istringstream iss(input);
+    string word;
 
-    std::uniform_real_distribution<float> distribution(min, max);
+    while (iss >> word) {
+        words.push_back(word);
+    }
+
+    return words;
+}
+
+
+float RandomFloat(float min, float max) {
+    random_device rd;
+    mt19937 gen(rd());
+
+    uniform_real_distribution<float> distribution(min, max);
 
     float random_number = distribution(gen);
 
@@ -97,36 +103,74 @@ float RandomFloat(float min, float max) {
 }
 
 int RandomInt(int min, int max) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    random_device rd;
+    mt19937 gen(rd());
 
-    std::uniform_int_distribution<int> distribution(min, max);
+    uniform_int_distribution<int> distribution(min, max);
 
     int random_number = distribution(gen);
 
     return random_number;
 }
 
-string ToLowerCase(string string)
+string ToLowerCase(string stringIn)
 {
-    std::string lowerString;
+    string lowerString;
     
-    for (char& c : string) {
-        c = std::tolower(c);
+    for (char& c : stringIn) {
+        c = tolower(c);
         lowerString = lowerString + c;
     }
 
     return lowerString;
 }
 
-string ToUpperCase(string string)
+string ToUpperCase(string stringIn)
 {
-    std::string upperString;
+    string upperString;
 
-    for (char& c : string) {
-        c = std::toupper(c);
+    for (char& c : stringIn) {
+        c = toupper(c);
         upperString = upperString + c;
     }
 
     return upperString;
+}
+
+float CompareStringByChar(const string& string1, const string& string2) { // Broken: Always returns 100.0
+    vector<char> string1Chars(string1.begin(), string1.end());
+    vector<char> string2Chars(string2.begin(), string2.end());
+
+    int matchingChars = 0;
+
+    for (char c1 : string1Chars) {
+        for (char c2 : string2Chars) {
+            if (c1 == c2) {
+                matchingChars++;
+                break;
+            }
+        }
+    }
+
+    float similarityPercent = (2.0f * matchingChars) / (string1.size() + string2.size()) * 100.0f;
+    return similarityPercent;
+}
+
+float CompareStringByWord(const string& string1, const string& string2) { // Broken: Always returns 100.0
+    vector<string> words1 = StringToList(string1);
+    vector<string> words2 = StringToList(string2);
+
+    int commonWords = 0;
+
+    for (const string& word1 : words1) {
+        for (const string& word2 : words2) {
+            if (word1 == word2) {
+                commonWords++;
+                break;
+            }
+        }
+    }
+
+    float similarityPercent = (2.0f * commonWords) / (words1.size() + words2.size()) * 100.0f;
+    return similarityPercent;
 }
